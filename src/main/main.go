a860762cecb7016a0aeb29503069bedb90c69619
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-03-01 00:41:44 -0800

    Reformat go code

--- src/main/main.go
+++ src/main/main.go
@@ -4,16 +4,16 @@ import "fmt"
 import "net/http"
 
 func main() {
-  var resp, err = http.Get("http://localhost:5984")
-  if err != nil {
-    fmt.Println("couchdb: not running on port 5984")
+	var resp, err = http.Get("http://localhost:5984")
+	if err != nil {
+		fmt.Println("couchdb: not running on port 5984")
 		fmt.Println("")
 		fmt.Println("    brew install couchdb")
 		fmt.Println("    ln -sfv /usr/local/opt/couchdb/*.plist ~/Library/LaunchAgents")
 		fmt.Println("    launchctl load ~/Library/LaunchAgents/homebrew.mxcl.couchdb.plist")
 		fmt.Println("")
-  } else {
-    defer resp.Body.Close()
-    fmt.Println("couchdb: OK")
-  }
+	} else {
+		defer resp.Body.Close()
+		fmt.Println("couchdb: OK")
+	}
 }

959764c984b09f4c998532cc5766d66256962bc2
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-03-01 11:23:04 -0800

    Use codegansta/cli for argument parsing

--- src/main/main.go
+++ src/main/main.go
@@ -1,19 +1,29 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"net/http"
+	"os"
+)
 
 func main() {
-	var resp, err = http.Get("http://localhost:5984")
-	if err != nil {
-		fmt.Println("couchdb: not running on port 5984")
-		fmt.Println("")
-		fmt.Println("    brew install couchdb")
-		fmt.Println("    ln -sfv /usr/local/opt/couchdb/*.plist ~/Library/LaunchAgents")
-		fmt.Println("    launchctl load ~/Library/LaunchAgents/homebrew.mxcl.couchdb.plist")
-		fmt.Println("")
-	} else {
-		defer resp.Body.Close()
-		fmt.Println("couchdb: OK")
+	app := cli.NewApp()
+	app.Name = "allup"
+	app.Action = func(c *cli.Context) {
+		var resp, err = http.Get("http://localhost:5984")
+		if err != nil {
+			fmt.Println("couchdb: not running on port 5984")
+			fmt.Println("")
+			fmt.Println("    brew install couchdb")
+			fmt.Println("    ln -sfv /usr/local/opt/couchdb/*.plist ~/Library/LaunchAgents")
+			fmt.Println("    launchctl load ~/Library/LaunchAgents/homebrew.mxcl.couchdb.plist")
+			fmt.Println("")
+		} else {
+			defer resp.Body.Close()
+			fmt.Println("couchdb: OK")
+		}
 	}
+
+	app.Run(os.Args)
 }
